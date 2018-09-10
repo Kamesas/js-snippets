d83c1ce470e0b3c9c4ed206595f6219ad03ea757
Kamesas <semakaleksandr2014@gmail.com>
2018-09-10 12:32:20 +0300

    clean

--- ip.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-	sleep(5);
-  echo $_SERVER['REMOTE_ADDR']
-
-?>
--- js/common.js
+++ js/common.js
@@ -1,29 +1,3 @@
-window.onload = function () {
-
-	document.querySelector('#myIP').onclick = function() {
-		//ajaxGet('../ip.php');
-		ajaxGet('../phones.json');
-	
-	}
-	
-}
-
-function ajaxGet(url) {
-
-	let request = new XMLHttpRequest() ;
-
-	request.onreadystatechange = function() {
-		console.log(request.readyState);
-
-		if(request.readyState == 4 && request.status == 200) {
-			let phones = 	document.querySelector('#myIP').innerHTML =	request.responseText;
-			console.log(phones);
-		}
-
-	}
-
-	
-	request.open('GET', url);
-	request.send();
-
-}
+window.onload = function() {
+  console.log("ready!");
+};
