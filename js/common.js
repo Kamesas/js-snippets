13dbdb64109bc88a6cb5d8115286d365d97df8a6
Kamesas <semakaleksandr2014@gmail.com>
2018-09-10 14:01:54 +0300

    addEventListener func

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 window.onload = function() {
-  document.body.addEventListener("click", function() {
-    console.log(1);
-  });
+  document.body.addEventListener("click", firstClick);
   document.body.addEventListener("click", function() {
     console.log(2);
   });
 };
+
+function firstClick(e) {
+  console.log(1);
+}
