4e5c34efbac4393b5ffbdc6a60ae0b8a1da5e78a
Kamesas <semakaleksandr2014@gmail.com>
2018-09-10 11:36:55 +0300

    slider ready

--- js/common.js
+++ js/common.js
@@ -1,11 +1,34 @@
 window.onload = function () {
 
 	const textColor = document.querySelector('#textColor');
-
 	textColor.onclick = function() {
 		this.style.color = "red"; 
 		this.innerHTML += ' from JS';
 		this.className = 'test';
+	}
+
+	const btnPrev = document.querySelector('#controlsBTN .prev');
+	const btnNext = document.querySelector('#controlsBTN .next');
+	const images = document.querySelectorAll('#foto img');
+	let i = 0;
+
+	btnPrev.onclick = function () {
+		images[i].style.display = 'none';
+		i--;
+		if (i < 0) {
+			i = images.length - 1;
+		}
+		images[i].style.display = 'block';		
+	}
+
+	btnNext.onclick = function () {
+		images[i].style.display = 'none';
+		i++;
+		if (i >= images.length) {
+			i = 0
+		}
+		images[i].style.display = 'block';		
+		
 	}
 
 
