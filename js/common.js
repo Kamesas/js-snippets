2ab553b7a7f21d0e1c40b270f6bb4686c186a387
Kamesas <semakaleksandr2014@gmail.com>
2018-09-11 15:22:51 +0300

    func CALL

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
 window.onload = function() {
-  console.log("ready!");
+  const items = document.querySelectorAll(".items .item");
+
+  // for (let i = 0; i < items.length; i++) {
+  //   items[i].onclick = adBd;
+  // }
+
+  function adBd() {
+    this.classList.toggle("item-active");
+  }
+
+  setInterval(function() {
+    let rand = mtRand(0, items.length - 1);
+    adBd.call(items[rand]);
+  }, 500);
+
+  function mtRand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1));
+  }
 };
