18f18cf73bba8a09b663447e72df1cb948a4bedb
Kamesas <semakaleksandr2014@gmail.com>
2018-09-11 17:50:50 +0300

    blicking with setInterval ready

--- js/common.js
+++ js/common.js
@@ -1,18 +1,22 @@
 window.onload = function() {
   const items = document.querySelectorAll(".items .item");
-
-  // for (let i = 0; i < items.length; i++) {
-  //   items[i].onclick = adBd;
-  // }
+  const start = document.querySelector(".start");
+  let blick = true;
 
   function adBd() {
     this.classList.toggle("item-active");
   }
 
-  setInterval(function() {
-    let rand = mtRand(0, items.length - 1);
-    adBd.call(items[rand]);
-  }, 500);
+  start.onclick = function() {
+    blick = !blick;
+    !blick ? (start.innerHTML = "Pause") : (start.innerHTML = "Start");
+    blick
+      ? clearInterval(setInterv)
+      : (setInterv = setInterval(function() {
+          let rand = mtRand(0, items.length - 1);
+          adBd.call(items[rand]);
+        }, 500));
+  };
 
   function mtRand(min, max) {
     return Math.floor(Math.random() * (max - min + 1));
