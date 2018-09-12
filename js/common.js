6abaec73fd3f5d6444b0bc2d9597e767ea91d8ee
Kamesas <semakaleksandr2014@gmail.com>
2018-09-12 22:43:44 +0300

    start OOP

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-  console.log("ready!");
+  let student1 = new Student("Alex", 30);
+  let student2 = new Student("Alla");
+
+  console.log(student1);
+  console.log(student2);
+
+  function Student(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.skill = "webDev";
+  }
 };
